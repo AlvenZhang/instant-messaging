30dbeb48c3c2724481d85981352433de4365f895
xifeng_alven <xifeng_alven@foxmail.com>
2025-11-12 23:59:27 +0800

    init

---
+++ src/test/java/com/message/instantmessaging/InstantMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.message.instantmessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantMessagingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
